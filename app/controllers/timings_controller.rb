b20f2a014d827d64b910d218bbece140e07f2dd9
Tom Coleman <tom@thesnail.org>
2012-02-28 13:00:12 +1100

    Added extra test for css namespacing

--- app/controllers/timings_controller.rb
+++ app/controllers/timings_controller.rb
@@ -19,6 +19,7 @@ private
       :class_inapplicable => 'Base, plus 10k styles that apply to non-existent classes',
       :general_inapplicable => 'Base, plus 10k styles which rightmost target divs',
       :specific_inapplicable => 'Base, plus 10k styles which rightmost target spans',
+      :namespaced_general_inapplicable => 'Base, plus 10k styles which rightmost target divs, except they cannot apply',
       :one_rule_many_selectors => 'One big rule that targest each of the 10k divs seperately',
       :one_rule_many_selectors_and_rules => 'The same rule repeated for each of the 10k divs',
       :big_styles => 'Base, plus 1k styles that have about 10 rules each'
--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,8 @@ module CssTimings
     config.assets.version = '1.0'
     
     config.assets.precompile += ['runner.css', 'empty.css', 'base.css',
-      'class_inapplicable.css', 'general_inapplicable.css', 'specific_inapplicable.css', 
+      'class_inapplicable.css', 'general_inapplicable.css', 'namespaced_general_inapplicable',
+      'specific_inapplicable.css', 
       'one_rule_many_selectors.css', 'one_rule_many_selectors_and_rules.css', 'big_styles.css',
       'runner.js', 'domready.js']
     
